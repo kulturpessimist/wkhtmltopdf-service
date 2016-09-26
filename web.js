f5f5ad52b59e74719884a8fa9664b15ba6bc28ff
Alex Schedler <con.sume.org@gmail.com>
2016-09-26 11:52:31 +0200

    cleanup and updates

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 // _require_ all the libraries!
 var express = require('express'),
+	mO 		= require('method-override'),
+	bP 		= require('body-parser'),
+
 	crypto	= require('crypto'),
 	exec	= require('child_process').exec,
 	os		= require('os');
-// on mac os x - our dev environment - the binary is in Applications... 
+// on mac os x - our dev environment - the binary is in $path... 
 // in linux we use our own binary 
 var wkhtmltopdf = {
-		'darwin': '/Applications/wkhtmltopdf.app/Contents/MacOS/wkhtmltopdf',
+		'darwin': 'wkhtmltopdf',
 		'linux':  './bin/wkhtmltopdf-amd64',
 		'linux2': './bin/wkhtmltopdf-amd64'
 	};
@@ -14,62 +17,70 @@ var wkhtmltopdf = {
 // on heroku we host 4 different versions of wkhtmltopdf
 // on darwin we just use wkhtmltopdf.app
 var versions = {
-		'darwin':'',
-		'default': '.0.12.3',
+		'darwin': 		'',
+		'default': 		'.0.12.3',
 		//
-		'0.9.9': '.0.9.9',
-		'0.9.6': '.0.9.6',
-		'0.10.0_beta5': '.0.10.0_beta5',
-		'0.11.0rc1': '.0.11.0rc1',
-		'0.12.3': '.0.12.3'
+		'0.9.9': 		'.0.9.9',
+		'0.11.0rc1': 	'.0.11.0rc1',
+		'0.12.3': 		'.0.12.3'
 	};
-
-	// a server with express... ah such a nice library!
-var app = express.createServer(express.logger());
+// the actual function to render the PDF
+var renderPDF = function(url, cb, params){
+	var version = (os.platform()=='darwin'?'darwin':'default');
+	var params = params || '-B 0 -L 0 -R 0 -T 0 -O Portrait -q --image-quality 80 --disable-external-links --print-media-type --javascript-delay 350';
+	// lets hash the url so we have a unique filename
+	var sha1_hash = crypto.createHash('sha1').update(url);
+	var name = sha1_hash.digest('hex');
+	// lets exec the wkhtmltopdf command
+	// but first get a timestamp to messure the performance
+	var start_time = new Date().getTime();
+	// this is where the magic happens... lol
+	var child = exec(
+		wkhtmltopdf[os.platform()] + versions[version] + ' ' + params + ' ' + url + ' ./assets/' + name + '.pdf', 
+		// heroku gives us 30 seconds, so we do it in 20 seconds
+		{ timeout: 29000, killSignal: 'SIGTERM' },
+		function (error, stdout, stderr) {
+			if (error !== null) {
+				// is everything goes wrong!
+				// show the user/client the error and leave him alone
+				console.log('exec error: ' + error);
+				//response.send('exec error: '+ JSON.stringify(error));
+				cb(error, null);
+			}else{
+				// ah, great it worked!
+				// how much time did we need? log it to console
+				console.log( '###', 'processing took', (new Date().getTime())-start_time, 'ms' );
+				// send the user/client to the asset
+				//response.redirect('/assets/'+name+'.pdf');
+				cb(null, name);
+			}
+		}
+	);
+}
+// a server with express... ah such a nice library!
+var app = express();
 // _configure_ all the parameters!
-app.configure(function(){
-	app.use(express.methodOverride());
-	// static files for our "frontend"
-	app.use(express.static(__dirname + '/public'));
-	// static files for our assets. 
-	// remember: files on heroku are just temporary -> http://goo.gl/5qWdt
-	app.use('/assets', express.static(__dirname + '/assets'));
-	// and last but not least the body Parser for our post route
-	app.use(express.bodyParser());
-});
+app.use(mO());
+// static files for our "frontend"
+app.use(express.static(__dirname + '/public'));
+// static files for our assets. 
+// remember: files on heroku are just temporary -> http://goo.gl/5qWdt
+app.use('/assets', express.static(__dirname + '/assets'));
+// and last but not least the body Parser for our post route
+app.use(bP());
 // our namespace sould be api
 app.get('/api/:url?', function(request, response) {
 	var url = request.params.url || '';
-	var version = (os.platform()=='darwin'?'darwin':'default');
-	// some flags for nicer PDFs
-	var flags = '-B 0 -L 0 -R 0 -T 0 -O Portrait -q --image-quality 80 --disable-external-links --print-media-type --javascript-delay 350';
 	// do we have a url?
 	if(url !== '' ){
-		// lets hash the url so we have a unique filename
-		var sha1_hash = crypto.createHash('sha1').update(url);
-		var hex = sha1_hash.digest('hex');
-		// lets exec the wkhtmltopdf command
-		// but first get a timestamp to messure the performance
-		var start_time = new Date().getTime();
-		// this is where the magic happens... lol
-		var child = exec(wkhtmltopdf[os.platform()]+versions[version]+' '+flags+" "+url+' ./assets/'+hex+'.pdf', 
-			// heroku gives us 30 seconds, so we do it in 20 seconds
-			{ timeout: 20000, killSignal: 'SIGTERM' },
-			function (error, stdout, stderr) {
-				if (error !== null) {
-					// is everything goes wrong!
-					// show the user/client the error and leave him alone
-					console.log('exec error: ' + error);
-					response.send('exec error: '+ JSON.stringify(error));
-				}else{
-					// ah, great it worked!
-					// how much time did we need? log it to console
-					console.log( '###', 'processing took', (new Date().getTime())-start_time, 'ms' );
-					// send the user/client to the asset
-					response.redirect('/assets/'+hex+'.pdf');
-				}
+		renderPDF(url, function(error, success){
+			if (error !== null) {
+				console.log('exec error: ' + error);
+				response.send('exec error: '+ JSON.stringify(error));		
+			}else{
+				response.redirect('/assets/'+success+'.pdf');
 			}
-		);
+		}, null);
 	}else{
 		// hmmm, no url? than go back to begin
 		response.redirect('/');
